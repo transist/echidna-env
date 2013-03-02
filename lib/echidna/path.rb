47dc45fcbab069018931d2dacaed2a33935bb4de
Richard Huang <flyerhzm@gmail.com>
2013-03-02 19:05:57 +0800

    require more dependencies

--- lib/echidna/path.rb
+++ lib/echidna/path.rb
@@ -6,7 +6,3 @@ path = path.sub('\/bin$', '')
 
 $app_root = Pathname.new(path)
 $app_name = path.split('-').last
-
-%w(lib/helpers/*.rb lib/**/*.rb app/models/*.rb config/**/*.rb app/apis/*.rb).each do |path|
-  Dir[$app_root.join(path)].each { |file| require_relative file }
-end
