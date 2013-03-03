d186d169393bfaf9bc88ee7d7f7d74c340c7dc21
Richard Huang <flyerhzm@gmail.com>
2013-03-03 20:29:40 +0800

    automatically get redis namespace

--- lib/echidna/redis.rb
+++ lib/echidna/redis.rb
@@ -5,7 +5,7 @@ require 'redis-namespace'
 
 redis_host = ENV['ECHIDNA_REDIS_HOST'] || "127.0.0.1"
 redis_port = ENV['ECHIDNA_REDIS_PORT'] || "6379"
-redis_namespace = ENV['ECHIDNA_REDIS_NAMESPACE'] || "e:d"
+redis_namespace = "e:#{ENV['USER']}:#{ENV['ECHIDNA_ENV'][0]}"
 
 $redis = Redis::Namespace.new(redis_namespace, redis: Redis.new(host: redis_host, port: redis_port, driver: "hiredis"))
 
