afbf3625fcd7e7dd17c287c8434886a71e29a614
Richard Huang <flyerhzm@gmail.com>
2013-03-04 22:18:10 +0800

    add echidna/port

--- lib/echidna/all.rb
+++ lib/echidna/all.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
 require_relative 'path'
+require_relative 'port'
 require_relative 'logger'
 require_relative 'redis'
 require_relative 'dependency'
---
+++ lib/echidna/port.rb
@@ -0,0 +1,24 @@
+USERS_OFFSET = {
+  echidna: 0, // production
+  staging: 1,
+  testing: 2,
+  development: 3,
+  rngadam: 4,
+  flyerhzm: 5,
+  rainux: 6,
+  simsicon: 7
+}
+
+BASE_PORT = {
+  'spider' => 62200,
+  'streaming' => 62300,
+  'dicts' => 62400,
+  'monitoring' => 62600,
+  'api' => 62700,
+  'deploy' => 62800
+};
+
+user = ENV['USER']
+offset = USERS_OFFSET[user] || USERS_OFFSET['development'];
+
+ENV["ECHIDNA_#{$app_name.upcase}_PORT"] = BASE_PORT[$app_name] + offset;
