dfdf6542d6201117e09578b747a32a7dcedb1b85
Rainux Luo <rainux@gmail.com>
2013-03-04 13:19:59 +0800

    Load dependencies recursively

--- lib/echidna/dependency.rb
+++ lib/echidna/dependency.rb
@@ -1,5 +1,13 @@
 # coding: utf-8
 
-%w(config/initializers/*.rb lib/redis/*.rb lib/helpers/*.rb lib/models/*.rb app/models/*.rb app/apis/*.rb app/plugins/*.rb).each do |path|
+%w(
+  config/initializers/**/*.rb
+  lib/redis/**/*.rb
+  lib/helpers/**/*.rb
+  lib/models/**/*.rb
+  app/models/**/*.rb
+  app/apis/**/*.rb
+  app/plugins/**/*.rb
+).each do |path|
   Dir[$app_root.join(path)].each { |file| require_relative file }
 end
